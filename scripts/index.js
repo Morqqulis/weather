07c2dd02f8fc4558d7661de64b9d52a24ffdec9d
Dmitriy <84258572+Morqqulis@users.noreply.github.com>
2024-04-26 01:46:39 +0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -10,7 +10,7 @@ const setCountry = e => (country = e.target.value)
 const getWeatherData = async () => {
     try {
         if (country) {
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${WEATHER_API_KEY}`)
+            const response = await fetch(WEATHER_URL)
             const data = await response.json()
             console.log(data)
             renderWeather(data)
